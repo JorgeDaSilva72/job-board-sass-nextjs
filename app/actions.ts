a5e16f7eabf7f7c52dcfe2b4c4146c7aa9ecc820
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-02-09 11:52:58 +0100

    feat: implemented Arcjet, our server actions are protected.

--- app/actions.ts
+++ app/actions.ts
@@ -8,34 +8,34 @@ import { redirect } from "next/navigation";
 // import { stripe } from "./utils/stripe";
 // import { jobListingDurationPricing } from "./utils/pricingTiers";
 import { revalidatePath } from "next/cache";
-// import arcjet, { detectBot, shield } from "./utils/arcjet";
-// import { request } from "@arcjet/next";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
 // import { inngest } from "./utils/inngest/client";
 
-// const aj = arcjet
-//   .withRule(
-//     shield({
-//       mode: "LIVE",
-//     })
-//   )
-//   .withRule(
-//     detectBot({
-//       mode: "LIVE",
-//       allow: [],
-//     })
-//   );
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  );
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const user = await requireUser();
 
-  // // Access the request object so Arcjet can analyze it
-  // const req = await request();
-  // // Call Arcjet protect
-  // const decision = await aj.protect(req);
+  // Access the request object so Arcjet can analyze it
+  const req = await request();
+  // Call Arcjet protect
+  const decision = await aj.protect(req);
 
-  // if (decision.isDenied()) {
-  //   throw new Error("Forbidden");
-  // }
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
 
   // Server-side validation
   const validatedData = companySchema.parse(data);
@@ -63,14 +63,14 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const user = await requireUser();
 
-  // Access the request object so Arcjet can analyze it
-  // const req = await request();
-  // Call Arcjet protect
-  // const decision = await aj.protect(req);
+  //Access the request object so Arcjet can analyze it
+  const req = await request();
+  //Call Arcjet protect
+  const decision = await aj.protect(req);
 
-  // if (decision.isDenied()) {
-  //   throw new Error("Forbidden");
-  // }
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
 
   const validatedData = jobSeekerSchema.parse(data);
 
