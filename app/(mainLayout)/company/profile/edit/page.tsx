59c949b39a23e4f25c1e5e41c5dbd2d04ed277b3
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-03-02 14:53:05 +0100

    fix : errors

--- app/(mainLayout)/company/profile/edit/page.tsx
+++ app/(mainLayout)/company/profile/edit/page.tsx
@@ -4,6 +4,9 @@ import EditCompanyForm from "@/components/forms/EditCompanyForm";
 import { notFound } from "next/navigation";
 import React from "react";
 
+// 🚀 Force Next.js à utiliser le rendu dynamique
+export const dynamic = "force-dynamic";
+
 const EditCompanyPage = async () => {
   const company = await getCompanyProfile();
 
