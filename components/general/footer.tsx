6eb5d1813e0a4783cb28d2e888d70bb5ea2dadb4
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2025-04-27 13:01:43 +0200

    feat(footer): internationalize FooterSection with next-intl
    - Add i18n support for footer content
    - Implement English and French translations
    - Maintain all social media links and functionality
    - Keep responsive layout and styling
    - Add type safety for translation keys

--- components/general/footer.tsx
+++ components/general/footer.tsx
@@ -131,6 +131,173 @@
 //   );
 // };
 // import DiscordIcon from "@/components/icons/discord-icon";
+
+// import { Separator } from "@/components/ui/separator";
+// import {
+//   Linkedin,
+//   Twitter,
+//   Instagram,
+//   Facebook,
+//   Youtube,
+//   Twitch,
+//   SmartphoneIcon,
+//   GlobeIcon,
+// } from "lucide-react";
+// import Image from "next/image";
+// import { Link } from "@/i18n/navigation";
+
+// export const FooterSection = () => {
+//   return (
+//     <footer id="footer" className="container py-8 sm:py-16">
+//       <div className="p-10 bg-card border border-secondary rounded-2xl">
+//         <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+//           {/* Logo Section */}
+//           <div className="col-span-full xl:col-span-2">
+//             <Link
+//               href="https://afrique-avenir.com/"
+//               className="flex items-center space-x-3"
+//             >
+//               <Image
+//                 src="/footer/logo-topaz-enhance-coupe.jpeg"
+//                 alt="Icone"
+//                 width={36}
+//                 height={36}
+//                 className="rounded-lg"
+//               />
+//               <h2 className="text-transparent text-2xl font-bold bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
+//                 AFRIQUE AVENIR
+//               </h2>
+//             </Link>
+//           </div>
+
+//           {/* Contact Section */}
+//           <div className="flex flex-col gap-3">
+//             <h3 className="font-bold text-lg">Contact</h3>
+//             <Link
+//               href="#footer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <Linkedin className="w-5 h-5" />
+//               LinkedIn
+//             </Link>
+//             <Link
+//               href="https://x.com/afriqueavenir9?s=21"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <Twitter className="w-5 h-5" />
+//               Twitter
+//             </Link>
+//             <Link
+//               href="https://m.twitch.tv/afriqueavenir/home"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <Twitch className="w-5 h-5" />
+//               Twitch
+//             </Link>
+//           </div>
+
+//           {/* Plateformes Section */}
+//           <div className="flex flex-col gap-3">
+//             <h3 className="font-bold text-lg">Plateformes</h3>
+//             <Link
+//               href="#footer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <SmartphoneIcon className="w-5 h-5" />
+//               iOS
+//             </Link>
+//             <Link
+//               href="#footer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <SmartphoneIcon className="w-5 h-5" />
+//               Android
+//             </Link>
+//             <Link
+//               href="#footer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <GlobeIcon className="w-5 h-5" />
+//               Web
+//             </Link>
+//           </div>
+
+//           {/* Aide Section */}
+//           <div className="flex flex-col gap-3">
+//             <h3 className="font-bold text-lg">Aide</h3>
+//             <Link href="#contact" className="opacity-60 hover:opacity-100">
+//               Nous contacter
+//             </Link>
+//             <Link href="#faq" className="opacity-60 hover:opacity-100">
+//               FAQ
+//             </Link>
+//             <Link href="#contact" className="opacity-60 hover:opacity-100">
+//               Feedback
+//             </Link>
+//           </div>
+
+//           {/* Réseaux sociaux */}
+//           <div className="flex flex-col gap-3">
+//             <h3 className="font-bold text-lg">Réseaux sociaux</h3>
+//             <Link
+//               href="https://www.youtube.com/@Afriqueavenir-m5g"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <Youtube className="w-5 h-5" />
+//               YouTube
+//             </Link>
+//             <Link
+//               href="https://www.instagram.com/afriqueavenir9/profilecard/"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <Instagram className="w-5 h-5" />
+//               Instagram
+//             </Link>
+//             <Link
+//               href="https://www.facebook.com/profile.php?id=61568365453332&mibextid=wwXIfr&rdid=T1Uukh3inyp1lt68&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F19TQgmn4dJ%2F%3Fmibextid%3DwwXIfr"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               className="flex items-center gap-2 opacity-60 hover:opacity-100"
+//             >
+//               <Facebook className="w-5 h-5" />
+//               Facebook
+//             </Link>
+//           </div>
+//         </div>
+
+//         {/* Separator */}
+//         <Separator className="my-6" />
+
+//         {/* Footer Credits */}
+//         <section className="text-center">
+//           <h3 className="text-sm">
+//             &copy; 2024 Conçu et développé par
+//             <Link
+//               target="_blank"
+//               href="https://jds-portfolio.netlify.app/"
+//               className="text-primary transition-all border-primary hover:border-b-2 ml-1"
+//             >
+//               Jorge Da Silva
+//             </Link>
+//           </h3>
+//         </section>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// BEGIN 27/04/2025 compatible next-intl
+
+"use client";
+
 import { Separator } from "@/components/ui/separator";
 import {
   Linkedin,
@@ -144,8 +311,11 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 export const FooterSection = () => {
+  const t = useTranslations("Footer");
+
   return (
     <footer id="footer" className="container py-8 sm:py-16">
       <div className="p-10 bg-card border border-secondary rounded-2xl">
@@ -158,20 +328,20 @@ export const FooterSection = () => {
             >
               <Image
                 src="/footer/logo-topaz-enhance-coupe.jpeg"
-                alt="Icone"
+                alt={t("logoAlt")}
                 width={36}
                 height={36}
                 className="rounded-lg"
               />
               <h2 className="text-transparent text-2xl font-bold bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-                AFRIQUE AVENIR
+                {t("companyName")}
               </h2>
             </Link>
           </div>
 
           {/* Contact Section */}
           <div className="flex flex-col gap-3">
-            <h3 className="font-bold text-lg">Contact</h3>
+            <h3 className="font-bold text-lg">{t("sections.contact")}</h3>
             <Link
               href="#footer"
               className="flex items-center gap-2 opacity-60 hover:opacity-100"
@@ -199,9 +369,9 @@ export const FooterSection = () => {
             </Link>
           </div>
 
-          {/* Plateformes Section */}
+          {/* Platforms Section */}
           <div className="flex flex-col gap-3">
-            <h3 className="font-bold text-lg">Plateformes</h3>
+            <h3 className="font-bold text-lg">{t("sections.platforms")}</h3>
             <Link
               href="#footer"
               className="flex items-center gap-2 opacity-60 hover:opacity-100"
@@ -225,23 +395,23 @@ export const FooterSection = () => {
             </Link>
           </div>
 
-          {/* Aide Section */}
+          {/* Help Section */}
           <div className="flex flex-col gap-3">
-            <h3 className="font-bold text-lg">Aide</h3>
+            <h3 className="font-bold text-lg">{t("sections.help")}</h3>
             <Link href="#contact" className="opacity-60 hover:opacity-100">
-              Nous contacter
+              {t("links.contactUs")}
             </Link>
             <Link href="#faq" className="opacity-60 hover:opacity-100">
               FAQ
             </Link>
             <Link href="#contact" className="opacity-60 hover:opacity-100">
-              Feedback
+              {t("links.feedback")}
             </Link>
           </div>
 
-          {/* Réseaux sociaux */}
+          {/* Social Media Section */}
           <div className="flex flex-col gap-3">
-            <h3 className="font-bold text-lg">Réseaux sociaux</h3>
+            <h3 className="font-bold text-lg">{t("sections.socialMedia")}</h3>
             <Link
               href="https://www.youtube.com/@Afriqueavenir-m5g"
               target="_blank"
@@ -272,19 +442,17 @@ export const FooterSection = () => {
           </div>
         </div>
 
-        {/* Separator */}
         <Separator className="my-6" />
 
-        {/* Footer Credits */}
         <section className="text-center">
           <h3 className="text-sm">
-            &copy; 2024 Conçu et développé par
+            {t("copyright")}
             <Link
               target="_blank"
               href="https://jds-portfolio.netlify.app/"
               className="text-primary transition-all border-primary hover:border-b-2 ml-1"
             >
-              Jorge Da Silva
+              {t("developerName")}
             </Link>
           </h3>
         </section>
